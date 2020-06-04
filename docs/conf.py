e23167a60df343ee05890eb4ad8baee545f1614c
ericjwhitney <eric.j.whitney1@optusnet.com.au>
2020-06-04 22:25:28 +0800

    Initial commit.

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))  # Designed to be run from docs/
-sys.path.insert(0, os.path.abspath('..'))  # Designed to be run from docs/
 
 # -- Project information -----------------------------------------------------
 
